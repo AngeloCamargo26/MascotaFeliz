b406613617f60cad45218bf3e57270bff1ab2c88
Angelo Camargo <camargoangelo24@gmail.com>
2022-09-24 19:09:24 -0500

    reemplazo

---
+++ PracticaVeterinaria.App.Dominio/Entidades/prueba.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace PracticaVeterinaria.App.Dominio
+{
+    public class Propietario : Persona
+    {
+        public int cedula { get; set; }
+        public string direccion{ get; set; }
+        public string correoElectronico { get; set; }
+   
+
+    }
+}
